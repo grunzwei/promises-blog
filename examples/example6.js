c367da8d6276995b3cb43f4fadd42d95ac294c1d
grunzwei-fresh <nathan@codefresh.io>
2015-07-31 18:20:27 +0000

    fix example6 docs and export

--- examples/example6.js
+++ examples/example6.js
@@ -1,9 +1,17 @@
+
+/**
+ * @param: {number} augend - first operand to sum operation
+ * @param: {number} addend - second operand to sum operation
+ * @returns: {Promise} resolves with sum of augend and addend
+ */
 function promisedSum(augend, addend) {
   var deferred = Q.defer();
 
   setTimeout(function () {
     deferred.resolve(augend + addend);
-  },0)
+  }, 0);
 
   return deferred.promise;
 }
+
+module.exports = promisedSum;
