8ec050e24e91cb657ef7ef8c02e9e7eb5ac0c7d6
grunzwei-fresh <nathan@codefresh.io>
2015-07-31 18:42:12 +0000

    add case of failure to example 4, change throw with console.log

--- examples/example4.js
+++ examples/example4.js
@@ -16,20 +16,55 @@ try {
               })
             }
             catch (err) {
-              throw err;
+              console.log('error summing 5: ' + err);
             }
           })
         }
         catch (err) {
-          throw err;
+			console.log('error summing 4: ' + err);
         }
       })
     }
     catch (err) {
-      throw err;
+		console.log('error summing 3: ' + err);
     }
   })
 }
 catch (err) {
-  throw err;
+  console.log('error summing 1,2: ' + err);
+}
+
+try {
+  sum(1, 2, function(err, sumResult) {
+    if (err) throw err;
+    try {
+      sum(sumResult, 3, function(err, sumResult) {
+        if (err) throw err;
+        try {
+          throw new Error('this is an example of a possible failure');
+          sum(sumResult, 4, function (err, sumResult) {
+            if (err) throw err;
+            try {
+              sum(sumResult, 5, function (err, sumResult) {
+                if (err) throw err;
+                console.log('the sum is ' + sumResult);
+              })
+            }
+            catch (err) {
+              console.log('error summing 5: ' + err);
+            }
+          })
+        }
+        catch (err) {
+			console.log('as expected - deliberate error summing 4: ' + err);
+        }
+      })
+    }
+    catch (err) {
+		console.log('error summing 3: ' + err);
+    }
+  })
+}
+catch (err) {
+  console.log('error summing 1,2: ' + err);
 }
