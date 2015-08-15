4fe40a5ea14df5ac6e1882f650182f0e6122e7af
grunzwei-fresh <nathan@codefresh.io>
2015-08-15 20:34:16 +0000

    added example showing how difficult it can be to handle errors properly in callback mode

--- examples/nesting-async-callback.js
+++ examples/nesting-async-callback.js
@@ -1,38 +1,77 @@
 var sum = require('./async-sum-callback')
 
 describe('node standard async callback functions', function () {
-	it ('can be combined by nesting an invocation of an async function in the result callback of an async function, allowing the new callback closure to access the result', function (done) {
-		sum(1, 2, function(err, sumResult) {
-			if (err) {
-				done(err);
-			}
-			else {
-			  	sum(sumResult, 3, function(err, sumResult) {
-				    if (err) {
-				    	done(err);
-				    }
-				    else {
-						sum(sumResult, 4, function (err, sumResult) {
-					      if (err) {
-					      	done(err);
-					      }
-					      else {
-					      	sum(sumResult, 5, function (err, sumResult) {
-						        if (err) {
-						        	done(err);
-						        }
-						        else if (sumResult === 15) {
-						        	done();
-						        }
-						        else {
-						        	done(new Error('result was expected to be 15, but was ' + sumResult));
-						        }
-						    });			    
-					      }
-				  		});
-				    }				    
-			  	});
-			}
+	describe('can be combined by nesting an invocation of an async function in the result callback of an async function', function () {
+		it ('allowing the new callback closure to access the result', function (done) {
+			sum(1, 2, function(err, sumResult) {
+				if (err) {
+					done(err);
+				}
+				else {
+				  	sum(sumResult, 3, function(err, sumResult) {
+					    if (err) {
+					    	done(err);
+					    }
+					    else {
+							sum(sumResult, 4, function (err, sumResult) {
+						      if (err) {
+						      	done(err);
+						      }
+						      else {
+						      	sum(sumResult, 5, function (err, sumResult) {
+							        if (err) {
+							        	done(err);
+							        }
+							        else if (sumResult === 15) {
+							        	done();
+							        }
+							        else {
+							        	done(new Error('result was expected to be 15, but was ' + sumResult));
+							        }
+							    });			    
+						      }
+					  		});
+					    }				    
+				  	});
+				}
+			});
+		});
+		
+		it ('but this can make error handling a bit tricky', function (done) {
+			sum(1, 2, function(err, sumResult) {
+				if (err) {
+					done(err);
+				}
+				else {
+					//we deliberately plant an undefined here to cause an error
+				  	sum(sumResult, undefined, function(err, sumResult) {
+				  		//and the error is expected, but if this wasn't test code designed for this exception, we could easily miss this
+					    if (err) {
+					    	done();
+					    }
+					    else {
+							sum(sumResult, 4, function (err, sumResult) {
+						      if (err) {
+						      	done(err);
+						      }
+						      else {
+						      	sum(sumResult, 5, function (err, sumResult) {
+							        if (err) {
+							        	done(err);
+							        }
+							        else if (sumResult === 15) {
+							        	done();
+							        }
+							        else {
+							        	done(new Error('result was expected to be 15, but was ' + sumResult));
+							        }
+							    });			    
+						      }
+					  		});
+					    }				    
+				  	});
+				}
+			});
 		});
 	});
 });
